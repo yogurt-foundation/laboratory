d40bbeccd95df5d382d4b3adad08553ef4298db8
Loouis Low <loouis@gmail.com>
2020-04-15 06:46:36 +0800

    new laboratory is setting up and running

---
+++ README.md
@@ -0,0 +1,10 @@
+# Laboratory
+
+> A laboratory for implement and testing Yogurt CSS prototypes.
+
+### _prototypes
+
+
+---
+
+[MIT](https://github.com/yogurt-foundation/laboratory/blob/master/LICENSE)
---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const autoPrefixer = require('autoprefixer')
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const postCss = require('gulp-postcss')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')
+const sassGlob = require('gulp-sass-glob')
+
+const { version } = require('./package.json')
+
+
+// ...minify/preprocess scss
+
+// outputStyle: `expanded` for debugging,
+// `compressed` for production.
+
+const srcScssPath = 'src/laboratory.scss'
+const distCssPath = 'dist'
+gulp.task('sass-min', () => {
+  return gulp.src(srcScssPath)
+    .pipe(sassGlob())
+    .pipe(sass({ outputStyle: 'compressed' })
+      .on('error', sass.logError))
+    .pipe(postCss([autoPrefixer()]))
+    .pipe(rename('yogurt-laboratory.min.css'))
+    .pipe(gulp.dest(distCssPath))
+})
+
+
+gulp.task('sass-raw', () => {
+  return gulp.src(srcScssPath)
+    .pipe(sassGlob())
+    .pipe(sass({ outputStyle: 'expanded' })
+      .on('error', sass.logError))
+    .pipe(rename('yogurt-laboratory.css'))
+    .pipe(gulp.dest(distCssPath))
+})
+
+
+// ...watch
+const watchSrcScssPath = 'src/**/**/**/**/*.scss'
+gulp.task('watch', gulp.series([
+
+    'sass-raw',
+    'sass-min',
+
+  ], () => {
+
+    gulp.watch(watchSrcScssPath,
+      gulp.series([
+        'sass-raw',
+        'sass-min'
+      ])
+    )
+
+  })
+)
